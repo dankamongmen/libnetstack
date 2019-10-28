0b5d8e86a784074c6785ea5f54fcc427cc7cccb6
nick black <dankamongmen@gmail.com>
2019-10-28 15:34:52 -0400

    demo: create and destroy a netstack

--- src/bin/demo.c
+++ src/bin/demo.c
@@ -1,5 +1,16 @@
 #include <stdlib.h>
+#include <netstack.h>
 
 int main(void){
+  netstack_opts nopts;
+  memset(&nopts, 0, sizeof(nopts));
+  struct netstack* ns = netstack_create(&nopts);
+  if(ns == NULL){
+    return EXIT_FAILURE;
+  }
+  // FIXME
+  if(netstack_destroy(ns)){
+    return EXIT_FAILURE;
+  }
   return EXIT_SUCCESS;
 }
