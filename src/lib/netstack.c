9d78a010d77f5fba48cdf8d836689c115939f05f
nick black <dankamongmen@gmail.com>
2019-10-29 12:01:47 -0400

    netstack: set up libnl callback set

--- src/lib/netstack.c
+++ src/lib/netstack.c
@@ -12,8 +12,23 @@
 #include "netstack.h"
 
 typedef struct netstack {
-  struct nl_sock* nl; // netlink connection abstraction from libnl
+  struct nl_sock* nl;  // netlink connection abstraction from libnl
+  struct nl_cb* cbset; // libnl callback set
 } netstack;
+
+static int
+msg_handler(struct nl_msg* msg, void* vns){
+  const netstack* ns = vns;
+  fprintf(stderr, "Netlink msg %p %p\n", msg, ns);
+  return NL_OK; // FIXME
+}
+
+static int
+err_handler(struct sockaddr_nl* nla, struct nlmsgerr* nlerr, void* vns){
+  const netstack* ns = vns;
+  fprintf(stderr, "Netlink error %p %p %p\n", nla, nlerr, ns);
+  return NL_OK; // FIXME
+}
 
 static int
 netstack_init(netstack* ns){
@@ -26,6 +41,20 @@ netstack_init(netstack* ns){
     return -1;
   }
   if(nl_socket_add_memberships(ns->nl, RTNLGRP_LINK, NFNLGRP_NONE)){
+    nl_socket_free(ns->nl);
+    return -1;
+  }
+  if((ns->cbset = nl_cb_alloc(NL_CB_VERBOSE)) == NULL){
+    nl_socket_free(ns->nl);
+    return -1;
+  }
+  // Passes this netstack object to libnl. The nl_sock thus must be destroyed
+  // before the netstack itself is.
+  if(nl_cb_set(ns->cbset, NL_CB_VALID, NL_CB_CUSTOM, msg_handler, ns)){
+    nl_socket_free(ns->nl);
+    return -1;
+  }
+  if(nl_cb_err(ns->cbset, NL_CB_VERBOSE, err_handler, ns)){
     nl_socket_free(ns->nl);
     return -1;
   }
