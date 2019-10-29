dfac7d3dd83d3d24694b1747c1310783c51f9758
nick black <dankamongmen@gmail.com>
2019-10-29 12:48:27 -0400

    netstack: spawn a thread for recv

--- src/lib/netstack.c
+++ src/lib/netstack.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include <linux/if_link.h>
 #include <linux/netlink.h>
 #include <netlink/socket.h>
@@ -14,7 +15,21 @@
 typedef struct netstack {
   struct nl_sock* nl;  // netlink connection abstraction from libnl
   struct nl_cb* cbset; // libnl callback set
+  pthread_t tid;
 } netstack;
+
+// Sits on blocking nl_recvmsgs()
+static void*
+netstack_thread(void* vns){
+  netstack* ns = vns;
+  while(nl_recvmsgs_default(ns->nl) == 0){
+    printf("Got a netlink message!\n"); // FIXME
+  }
+  fprintf(stderr, "Error receiving from netlink socket (%s)\n",
+          strerror(errno));
+  // FIXME recover?
+  return NULL;
+}
 
 static int
 msg_handler(struct nl_msg* msg, void* vns){
@@ -55,6 +70,10 @@ netstack_init(netstack* ns){
     return -1;
   }
   if(nl_cb_err(ns->cbset, NL_CB_VERBOSE, err_handler, ns)){
+    nl_socket_free(ns->nl);
+    return -1;
+  }
+  if(pthread_create(&ns->tid, NULL, netstack_thread, ns)){
     nl_socket_free(ns->nl);
     return -1;
   }
