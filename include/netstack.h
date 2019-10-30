4249e317cdf517a7a0855472b09bba0a5ce069a4
nick black <dankamongmen@gmail.com>
2019-10-30 03:07:48 -0400

    netstack_*_attr: o(1) on most o(n) for forwards-compat #8

--- include/netstack.h
+++ include/netstack.h
@@ -24,26 +24,79 @@ extern "C" {
 
 struct netstack;
 
+// each of these types corresponds to a different rtnetlink message type. we
+// copy the payload directly from the netlink message to rtabuf, but that form
+// requires o(n) to get to any given attribute. we store a table of o(1)
+// pointers into this buffer at rta_indexed. if we're running on a newer
+// kernel, we might get an attribute larger than we're prepared to handle.
+// that's fine; interested parties can still extract it using rtnetlink(3)
+// macros. the convenience functions netstack_*_attr() are provided for this
+// purpose: each will retrieve the value via lookup if less than the MAX
+// against which we were compiled, and do an o(n) check otherwise.
 typedef struct netstack_iface {
   struct ifinfomsg ifi;
-  char name[IFNAMSIZ];
+  char name[IFNAMSIZ]; // NUL-terminated
+  void* rtabuf;        // copied directly from message
+  void* rta_indexed[__IFLA_MAX];
   // FIXME
 } netstack_iface;
 
+static inline void*
+netstack_iface_attr(const netstack_iface* ni, unsigned attridx){
+  if(attridx < sizeof(ni->rta_indexed) / sizeof(*ni->rta_indexed)){
+    return ni->rta_indexed[attridx];
+  }
+  // FIXME do o(n) check
+  return NULL;
+}
+
 typedef struct netstack_addr {
   struct ifaddrmsg ifa;
+  void* rtabuf;        // copied directly from message
+  void* rta_indexed[__IFA_MAX];
   // FIXME
 } netstack_addr;
+
+static inline void*
+netstack_addr_attr(const netstack_addr* na, unsigned attridx){
+  if(attridx < sizeof(na->rta_indexed) / sizeof(*na->rta_indexed)){
+    return na->rta_indexed[attridx];
+  }
+  // FIXME do o(n) check
+  return NULL;
+}
 
 typedef struct netstack_route {
   struct rtmsg rt;
   // FIXME
+  void* rtabuf;        // copied directly from message
+  void* rta_indexed[__RTA_MAX];
 } netstack_route;
+
+static inline void*
+netstack_route_attr(const netstack_route* nr, unsigned attridx){
+  if(attridx < sizeof(nr->rta_indexed) / sizeof(*nr->rta_indexed)){
+    return nr->rta_indexed[attridx];
+  }
+  // FIXME do o(n) check
+  return NULL;
+}
 
 typedef struct netstack_neigh {
   struct ndmsg nd;
   // FIXME
+  void* rtabuf;        // copied directly from message
+  void* rta_indexed[__NDA_MAX];
 } netstack_neigh;
+
+static inline void*
+netstack_neigh_attr(const netstack_neigh* nn, unsigned attridx){
+  if(attridx < sizeof(nn->rta_indexed) / sizeof(*nn->rta_indexed)){
+    return nn->rta_indexed[attridx];
+  }
+  // FIXME do o(n) check
+  return NULL;
+}
 
 // Callback types for various events. Even though routes, addresses etc. can be
 // reached through a netstack_iface object, they each get their own type of
