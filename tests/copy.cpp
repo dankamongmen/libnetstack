4d96ee6201e2eebc380fe33beb0d9e01f9c7e869
nick black <dankamongmen@gmail.com>
2019-11-01 07:18:46 -0400

    add unit test for copy iface #12

--- tests/copy.cpp
+++ tests/copy.cpp
@@ -29,7 +29,7 @@ TEST(CopyIface, IfaceDeepCopy) {
   struct netstack* ns = netstack_create(&nopts);
   ASSERT_NE(nullptr, ns);
   std::unique_lock<std::mutex> lck(cc.mlock);
-  cc.mcond.wait(lck, [&cc]{ fprintf(stderr, "IDX: %d\n", cc.idx); return cc.idx >= 0; });
+  cc.mcond.wait(lck, [&cc]{ return cc.idx >= 0; });
   netstack_iface* ni = netstack_iface_copy_byidx(ns, cc.idx);
   ASSERT_NE(nullptr, ni);
   char name[IFNAMSIZ];
@@ -39,6 +39,32 @@ TEST(CopyIface, IfaceDeepCopy) {
   char name2[IFNAMSIZ];
   ASSERT_EQ(name2, netstack_iface_name(ni2, name2));
   ASSERT_NE(ni, ni2);
+  ASSERT_STREQ(name, name2);
+  netstack_iface_abandon(ni2);
+  cc.mlock.unlock();
+  ASSERT_EQ(0, netstack_destroy(ns));
+  netstack_iface_abandon(ni); // we should still be able to use it
+}
+
+TEST(CopyIface, IfaceShare) {
+  struct copycurry cc = { .idx = -1 };
+  netstack_opts nopts;
+  memset(&nopts, 0, sizeof(nopts));
+  nopts.iface_cb = ifacecb;
+  nopts.iface_curry = &cc;
+  struct netstack* ns = netstack_create(&nopts);
+  ASSERT_NE(nullptr, ns);
+  std::unique_lock<std::mutex> lck(cc.mlock);
+  cc.mcond.wait(lck, [&cc]{ return cc.idx >= 0; });
+  const netstack_iface* ni = netstack_iface_share_byidx(ns, cc.idx);
+  ASSERT_NE(nullptr, ni);
+  char name[IFNAMSIZ];
+  ASSERT_EQ(name, netstack_iface_name(ni, name));
+  const netstack_iface* ni2 = netstack_iface_share_byidx(ns, cc.idx);
+  ASSERT_NE(nullptr, ni2);
+  char name2[IFNAMSIZ];
+  ASSERT_EQ(name2, netstack_iface_name(ni2, name2));
+  ASSERT_EQ(ni, ni2);
   ASSERT_STREQ(name, name2);
   netstack_iface_abandon(ni2);
   cc.mlock.unlock();
