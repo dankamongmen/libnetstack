0819686da300bdc93b9374e1e6e24be7387ee12e
nick black <dankamongmen@gmail.com>
2019-11-02 21:02:00 -0400

    unit tests for _ASYNC/_BLOCK/_NONE #17

--- tests/netstack.cpp
+++ tests/netstack.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include "main.h"
 
 TEST(Netstack, CreateNULLOpts) {
@@ -38,4 +39,39 @@ TEST(Netstack, RefuseCurryWithoutCB) {
   ns = netstack_create(&nopts);
   ASSERT_NE(nullptr, ns);
   ASSERT_EQ(0, netstack_destroy(ns));
+}
+
+void ifacecb(const netstack_iface* ni, netstack_event_e etype, void* curry) {
+  std::atomic<int>* post = static_cast<std::atomic<int>*>(curry);
+  ++*post;
+}
+
+TEST(Netstack, CreateInitialEventsNone) {
+  std::atomic<int> shouldnt_post{0};
+  netstack_opts nopts;
+  memset(&nopts, 0, sizeof(nopts));
+  nopts.initial_events = NETSTACK_INITIAL_EVENTS_NONE;
+  nopts.iface_cb = ifacecb;
+  nopts.iface_curry = &shouldnt_post;
+  struct netstack* ns = netstack_create(&nopts);
+  ASSERT_NE(nullptr, ns);
+  sleep(1);
+  ASSERT_EQ(0, netstack_destroy(ns));
+  ASSERT_EQ(0, shouldnt_post);
+}
+
+TEST(Netstack, CreateInitialEventsBlock) {
+  std::atomic<int> post{0};
+  netstack_opts nopts;
+  memset(&nopts, 0, sizeof(nopts));
+  nopts.initial_events = NETSTACK_INITIAL_EVENTS_BLOCK;
+  nopts.iface_cb = ifacecb;
+  nopts.iface_curry = &post;
+  struct netstack* ns = netstack_create(&nopts);
+  ASSERT_NE(nullptr, ns);
+  int postcopy = post;
+  ASSERT_NE(0, postcopy);
+  sleep(1);
+  ASSERT_EQ(0, netstack_destroy(ns));
+  ASSERT_EQ(postcopy, post);
 }
