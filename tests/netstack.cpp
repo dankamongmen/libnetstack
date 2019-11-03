6deeb0b68ded377e469025a4de620207c19d820e
nick black <dankamongmen@gmail.com>
2019-11-02 20:40:36 -0400

    test all four curries in RefuseCurryWithoutCB

--- tests/netstack.cpp
+++ tests/netstack.cpp
@@ -14,10 +14,28 @@ TEST(Netstack, CreateDefaultOpts) {
   ASSERT_EQ(0, netstack_destroy(ns));
 }
 
+// providing a curry without a corresponding callback ought result in failure
 TEST(Netstack, RefuseCurryWithoutCB) {
   netstack_opts nopts;
   memset(&nopts, 0, sizeof(nopts));
   nopts.iface_curry = &nopts;
   struct netstack* ns = netstack_create(&nopts);
   ASSERT_EQ(nullptr, ns);
+  nopts.iface_curry = NULL;
+  nopts.addr_curry = &nopts;
+  ns = netstack_create(&nopts);
+  ASSERT_EQ(nullptr, ns);
+  nopts.addr_curry = NULL;
+  nopts.route_curry = &nopts;
+  ns = netstack_create(&nopts);
+  ASSERT_EQ(nullptr, ns);
+  nopts.route_curry = NULL;
+  nopts.neigh_curry = &nopts;
+  ns = netstack_create(&nopts);
+  ASSERT_EQ(nullptr, ns);
+  nopts.neigh_curry = NULL;
+  // we ought now be able to create successfully
+  ns = netstack_create(&nopts);
+  ASSERT_NE(nullptr, ns);
+  ASSERT_EQ(0, netstack_destroy(ns));
 }
